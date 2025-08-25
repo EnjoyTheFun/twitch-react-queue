2eb4b34930dcd2ad58a28c307cce5b276039b32b
Enjoy The Fun <49270659+EnjoyTheFun@users.noreply.github.com>
2025-08-25 18:36:10 +0300

    Update to v1.0.5b
    Changelist:
    - Revert to using error handling for disposing video.js
    - Fix test error

--- src/features/clips/providers/kickClip/kickClipProvider.test.ts
+++ src/features/clips/providers/kickClip/kickClipProvider.test.ts
@@ -3,7 +3,7 @@ import kickClipProvider from './kickClipProvider';
 describe('KickClipProvider', () => {
   it('gets clip info from www.Kick.com url', () => {
     expect(kickClipProvider.getIdFromUrl('https://kick.com/silent/clips/clip_01JNVX1HA6QA593VKT6WEE5ZQE')).toEqual(
-      'clip_01JNVX1HA6QA593VKT6WEE5ZQE'
+      'silent|clip_01JNVX1HA6QA593VKT6WEE5ZQE'
     );
   });
 });
